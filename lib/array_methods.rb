b44e001f8eb2097c29972333f4497ce5a5dc65e9
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 03:25:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,11 @@
 # require 'pry'
 
+# def using_include(array, element)
+#   array.include?(element)
+# end
+
 def using_include(array, element)
-  array.include?(element)
+  array[element].include?
 end
 
 def using_sort(array)
